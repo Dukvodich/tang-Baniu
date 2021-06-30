c1867e69bd6a93537a5d286c4cd0d97e9ca92ecb
Dukvodich <86740361+Dukvodich@users.noreply.github.com>
2021-07-01 01:43:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
     question:'Trên thế giới hơn 7 tỉ người mà sao cậu lại chơi thân với tớ <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    mess: 'Tui bít mà 🥰. Yêu cậu nhiều nhiều 😘😘',
     messDesc: 'cậu đúng là',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
